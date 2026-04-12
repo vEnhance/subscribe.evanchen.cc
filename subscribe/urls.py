265b7c6cc267f5ba905fac42db24100635a76900
Evan Chen <evan@evanchen.cc>
2026-04-12 09:32:12 -0400

    feat: override allauth base template

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,9 +1,19 @@
+from allauth.socialaccount import views as socialaccount_views
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.socialaccount.urls")),
+    path(
+        "accounts/login/cancelled/",
+        socialaccount_views.login_cancelled,
+        name="socialaccount_login_cancelled",
+    ),
+    path(
+        "accounts/login/error/",
+        socialaccount_views.login_error,
+        name="socialaccount_login_error",
+    ),
     path("accounts/", include("allauth.socialaccount.providers.google.urls")),
     path("", include("mailing.urls")),
 ]
