a75d5f5e0dbad8233291ee6fdb8d33943b6d6e65
Evan Chen <evan@evanchen.cc>
2026-04-12 11:35:55 -0400

    drop: abandon magic link workflow altogether
    i think it's too much security risk to allow rando's to generate email
    addresses after the april 11 incident

--- mailing/urls.py
+++ mailing/urls.py
@@ -4,12 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new/", views.subscribe_form, name="subscribe_form"),
-    path(
-        "add/<str:token>/",
-        views.confirm_subscription,
-        name="confirm_subscription",
-    ),
     path(
         "remove/<str:token>/",
         views.unsubscribe_by_token,
--- mailing/views.py
+++ mailing/views.py
@@ -1,12 +1,6 @@
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
-from django.httip import HttpResponse
 from django.shortcuts import render
-from django.urls import reverse
-from postmarker.core import PostmarkClient
 
-from .forms import EmailSubscribeForm
 from .models import SubscriberEmail
 
 _DONE_TITLES = {
@@ -26,42 +20,6 @@ def index(request):
     return render(
         request, "mailing/index.html", {"page_title": "blog.evanchen.cc mailing list"}
     )
-
-
-def subscribe_form(request):
-    if not settings.POSTMARK_API_KEY:
-        return HttpResponse("Currently not available", status=503)
-    if request.method == "POST":
-        form = EmailSubscribeForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data["email"]
-            obj, _ = SubscriberEmail.objects.get_or_create(email=email)
-            if obj.subscribed is not None:
-                _send_confirmation_email(email, obj.token, request)
-            return render(
-                request,
-                "mailing/confirmation_sent.html",
-                {"page_title": "Check your email"},
-            )
-    else:
-        form = EmailSubscribeForm()
-    return render(
-        request,
-        "mailing/subscribe_form.html",
-        {"page_title": "Subscribe", "form": form},
-    )
-
-
-def confirm_subscription(request, token):
-    try:
-        obj = SubscriberEmail.objects.get(token=token)
-    except SubscriberEmail.DoesNotExist:
-        return render(request, "mailing/bad_token.html", {"page_title": "Link invalid"})
-    if request.method == "POST":
-        obj.subscribed = True
-        obj.save()
-        return _render_done(request, "subscribed", obj.email)
-    return _render_confirm(request, "subscribe", obj.email)
 
 
 def unsubscribe_by_token(request, token):
@@ -85,6 +43,9 @@ def oauth_subscribe(request):
         obj.google_authenticated = True
         obj.save()
         return _render_done(request, "subscribed", email)
+    elif SubscriberEmail.objects.filter(email=email, subscribed=True).exists():
+        return _render_done(request, "already_subscribed", email)
+
     return _render_confirm(request, "subscribe", email)
 
 
@@ -124,31 +85,4 @@ def _render_confirm(request, action, email):
             "action": action,
             "email": email,
         },
-    )
-
-
-def _send_confirmation_email(email, token, request):
-    if not settings.POSTMARK_API_KEY:
-        raise PermissionDenied
-    client = PostmarkClient(server_token=settings.POSTMARK_API_KEY)
-    confirm_url = request.build_absolute_uri(
-        reverse("confirm_subscription", args=[token])
-    )
-    unsub_url = request.build_absolute_uri(
-        reverse("unsubscribe_by_token", args=[token])
-    )
-    client.emails.send(
-        From=settings.POSTMARK_FROM_EMAIL,
-        To=email,
-        Subject="Confirm your subscription to blog.evanchen.cc",
-        TextBody=(
-            "Someone (hopefully you) asked to subscribe to blog.evanchen.cc."
-            f"Click the link below to confirm your subscription:\n{confirm_url}\n\n"
-            f"To unsubscribe later, visit:\n{unsub_url}"
-        ),
-        HtmlBody=(
-            "<p>Someone (hopefully you) asked to subscribe to blog.evanchen.cc.</p>"
-            f'<p><a href="{confirm_url}">Confirm subscription</a></p>'
-            f'<p>Later on, you can use <a href="{unsub_url}">unsubscribe here</a></p>'
-        ),
     )
