cd2414896c85732fe0fca17a31e2b6118866b9ad
Evan Chen <evan@evanchen.cc>
2026-04-12 10:52:26 -0400

    chore: add IS_PRODUCTION setting

--- subscribe/settings.py
+++ subscribe/settings.py
@@ -5,6 +5,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
+PRODUCTION = config("IS_PRODUCTION", default=False, cast=bool)
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
