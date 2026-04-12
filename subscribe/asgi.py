5a229edf9920c39bc5f6f930b8f2e83c0861fc41
Evan Chen <evan@evanchen.cc>
2026-04-12 08:48:16 -0400

    style: formatting, pass djlint/ruff

--- subscribe/asgi.py
+++ subscribe/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'subscribe.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subscribe.settings")
 
 application = get_asgi_application()
