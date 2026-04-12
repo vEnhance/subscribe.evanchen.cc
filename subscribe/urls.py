9961a05f7994155f50c4a7186bfaf5d5a105439e
Evan Chen <evan@evanchen.cc>
2026-04-12 09:18:03 -0400

    feat: limit social account URL's
    prevent traditional login

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
+    path("accounts/", include("allauth.socialaccount.urls")),
+    path("accounts/", include("allauth.socialaccount.providers.google.urls")),
     path("", include("mailing.urls")),
 ]
